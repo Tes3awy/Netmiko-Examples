bc7289e9f0dc2705a4ee12bdaf4c367f0defd095
Tes3awy <osaid2512@gmail.com>
2021-05-31 19:39:00 +0200

    New example

---
+++ netmiko/example4-2.py
@@ -0,0 +1,83 @@
+# Create an Excel sheet with Context Manager and save data from show version in
+
+import xlsxwriter
+from netmiko import ConnectHandler
+
+# Devices to SSH into
+devices = [
+    {
+        "device_type": "cisco_ios",
+        "ip": "sandbox-iosxe-latest-1.cisco.com",
+        "username": "developer",
+        "password": "C1sco12345",
+        "port": 22,
+    },
+    {
+        "device_type": "cisco_ios",
+        "ip": "ios-xe-mgmt.cisco.com",
+        "username": "developer",
+        "password": "C1sco12345",
+        "port": 8181,
+    },
+]
+
+# Create an Excel file
+with xlsxwriter.Workbook("Example4-2-Inventory-Details.xlsx") as workbook:
+    # Create an Excel sheet within the file
+    worksheet = workbook.add_worksheet("Inventory Details")
+    # Filters
+    worksheet.autofilter("A1:K1")
+
+    # Create Header cell for each entry
+    header = {
+        "A1": "Device Hostname",
+        "B1": "MGMT IP Address",
+        "C1": "Serial Number",
+        "D1": "MAC Address",
+        "E1": "Device Model",
+        "F1": "SW Version",
+        "G1": "SW Type",
+        "H1": "Operation Mode",
+        "I1": "Last Reload Reason",
+        "J1": "Device Up Time",
+        "K1": "Configuration Register",
+    }
+
+    # Loop over headers and create cells in first row (row 0)
+    for key, value in header.items():
+        worksheet.write(key, value)
+
+    # Starting values for row and column in the Excel workbook
+    row = 1
+    col = 0
+
+    # Loop over devices
+    for device in devices:
+        # Create a connection instance
+        with ConnectHandler(**device) as net_connect:
+            output = net_connect.send_command("show version", use_textfsm=True)
+
+        # Loop over each value in output variable and insert each value
+        # in the corresponding cell according to the header above
+        for value in output:
+            worksheet.write(row, col + 0, value["hostname"])
+            worksheet.write(row, col + 1, device["ip"])  # use IP from device variable
+            worksheet.write(row, col + 2, value["serial"][0])
+            try:
+                worksheet.write(row, col + 3, value["mac"][0])
+            except IndexError:
+                worksheet.write(row, col + 3, "Hidden")
+            worksheet.write(row, col + 4, value["hardware"][0])
+            worksheet.write(row, col + 5, value["version"])
+            worksheet.write(row, col + 6, value["rommon"])
+            if "bin" in value["running_image"]:
+                worksheet.write(row, col + 7, "BUNDLE")
+            else:
+                worksheet.write(row, col + 7, "INSTALL")
+            worksheet.write(row, col + 8, value["reload_reason"])
+            worksheet.write(row, col + 9, value["uptime"])
+            worksheet.write(row, col + 10, value["config_register"])
+            # Jump to next row
+            row += 1
+
+print("Done")
