d4651fbfa84250dce02a195a2eb5fa1d04928aea
Tes3awy <osaid2512@gmail.com>
2021-05-31 23:06:34 +0200

    Update example

--- netmiko/example10.py
+++ netmiko/example10.py
@@ -1,26 +1,28 @@
 # Detect unknown device types
 
+from pprint import pprint
 from netmiko import ConnectHandler, SSHDetect
 
 # Define a device
 device = {
-    "device_type": "autodetect",
+    "device_type": "autodetect",  # autodetect
     "ip": "sandbox-iosxe-latest-1.cisco.com",
     "username": "developer",
     "password": "C1sco12345",
-    "secret": "",
     "port": 22,
 }
 
-# Create an SSH connection instance
+# Create guessing variables
 guesser = SSHDetect(**device)
 best_match = guesser.autodetect()
+print("\nDevice Type Best Match:", best_match, "\n")
 
-# Override autodetect with the best match
+# Override autodetect value with the best match
 device["device_type"] = best_match
 
+# Create an SSH connection instance
 with ConnectHandler(**device) as net_connect:
-    output = net_connect.send_command("show version")
-print(output)
+    output = net_connect.send_command("show version", use_textfsm=True)
+pprint(output)
 
 print("Done")
