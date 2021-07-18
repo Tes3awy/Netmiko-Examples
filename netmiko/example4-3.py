f44a9a956af6f54377a80d2a355fb3627a8ca14b
tes3awy <osaid2512@gmail.com>
2021-07-18 22:02:32 +0200

    Fix black and isort conflict

--- netmiko/example4-3.py
+++ netmiko/example4-3.py
@@ -3,6 +3,7 @@
 # (Implicitly uses xlsxwriter to create the Excel file)
 
 import pandas as pd
+from pandas import ExcelWriter
 
 from netmiko import ConnectHandler
 
@@ -30,60 +31,61 @@ output = []
 # Command to send
 cmd = "show version"
 
-# Loop over all devices
-for device in devices:
-    # Create a connection instance to each device
-    with ConnectHandler(**device) as net_connect:
-        facts = net_connect.send_command(command_string=cmd, use_textfsm=True)
-    # Append the show command output to the `output` empty list
-    output.append(facts[0])
-
-# Create a data frame from the ouput list
-df = (
-    pd.DataFrame(output)
-    .reindex(
-        columns=[
-            "hostname",
-            "serial",
-            "mac",
-            "hardware",
-            "rommon",
-            "version",
-            "running_image",
-            "reload_reason",
-            "restarted",
-            "uptime",
-            "config_register",
-        ]
-    )
-    .rename(
-        columns={
-            "hostname": "Hostname",
-            "serial": "Serial Number",
-            "mac": "MAC Address",
-            "hardware": "Device Model",
-            "rommon": "Software Type",
-            "version": "Software Version",
-            "running_image": "Running Image",
-            "reload_reason": "Last Reload Reason",
-            "restarted": "Restarted",
-            "uptime": "Device Uptime",
-            "config_register": "Configuration Register",
-        }
-    )
-)
-
 # Name of exported excel file
 excel_file = "Example4-3-Inventory-Details-pandas.xlsx"
 
-# Export data to an Excel file using to_excel from Pandas
-df.to_excel(
-    excel_writer=excel_file,  # name of Excel file
-    index=False,  # remove automatically generated first index column
-    sheet_name="Device List using Pandas",
-    verbose=True,  # show verbose output for errors
-    freeze_panes=(1, 1),  # freeze top row & most left column
-    engine="xlsxwriter",  # the engine to create the Excel file
-)
+with ExcelWriter(path=excel_file) as writer:
+    # Loop over all devices
+    for device in devices:
+        # Create a connection instance to each device
+        with ConnectHandler(**device) as net_connect:
+            facts = net_connect.send_command(command_string=cmd, use_textfsm=True)
+        # Append the show command output to the `output` empty list
+        output.append(facts[0])
+
+    # Create a data frame from the ouput list
+    df = (
+        pd.DataFrame(output)
+        .reindex(
+            columns=[
+                "hostname",
+                "serial",
+                "mac",
+                "hardware",
+                "rommon",
+                "version",
+                "running_image",
+                "reload_reason",
+                "restarted",
+                "uptime",
+                "config_register",
+            ]
+        )
+        .rename(
+            columns={
+                "hostname": "Hostname",
+                "serial": "Serial Number",
+                "mac": "MAC Address",
+                "hardware": "Device Model",
+                "rommon": "Software Type",
+                "version": "Software Version",
+                "running_image": "Running Image",
+                "reload_reason": "Last Reload Reason",
+                "restarted": "Restarted",
+                "uptime": "Device Uptime",
+                "config_register": "Configuration Register",
+            }
+        )
+    )
+
+    # Export data to an Excel file using to_excel from Pandas
+    df.to_excel(
+        excel_writer=writer,  # name of Excel file
+        index=False,  # remove automatically generated first index column
+        sheet_name="Device List using Pandas",
+        verbose=True,  # show verbose output for errors
+        freeze_panes=(1, 1),  # freeze top row & most left column
+        engine="xlsxwriter",  # the engine to create the Excel file
+    )
 
 print("Done")
